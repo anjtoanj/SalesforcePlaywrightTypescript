8cfc2f278f7f5f9082f998c23b693f21ee3be994
Anju Sreekumar <93622823+anjtoanj@users.noreply.github.com>
2026-01-31 11:40:54 +1000

    Update users.ts

--- constants/users.ts
+++ constants/users.ts
@@ -1,7 +1,5 @@
 export const Users = {
   //Admin User
-  adminUserName: "Thanmayi321@gmail.com",
-  adminPassword: "HelloThanmayi@123",
-  // adminUserName: process.env.ADMIN_USERNAME!,
-  // adminPassword: process.env.ADMIN_PASSWORD!,
+  adminUserName: process.env.ADMIN_USERNAME!,
+  adminPassword: process.env.ADMIN_PASSWORD!,
 };
